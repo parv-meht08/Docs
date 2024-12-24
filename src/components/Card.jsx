e9124ece1242f63d171925dbd80d7480e1133c89
Parv Mehta <mehtaparv21@gmail.com>
2024-12-24 12:59:22 +0530

    Done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React from "react";
 import { FiFileText } from "react-icons/fi";
 import { MdOutlineFileDownload } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
+import { motion } from "framer-motion"
 
-const Card = ({ data }) => {
+const Card = ({ data, reference }) => {
   return (
-    <div>
-      <div className="relative w-60 h-72 rounded-[40px] bg-zinc-900 text-white px-8 py-10 overflow-hidden">
+    <motion.div>
+      <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} dragElastic={0.2 } className="relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900 text-white px-8 py-10 overflow-hidden">
         <FiFileText />
         <p className="text-sm leading-tight mt-5 font-semibold">{data.desc}</p>
         <div className="footer absolute bottom-0 w-full left-0">
@@ -20,12 +23,14 @@ const Card = ({ data }) => {
               )}
             </span>
           </div>
-          <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-            <h3 className="text-sm font-semibold text-white">Download Now</h3>
-          </div>
+          {data.tag.isOpen && (
+            <div className={`tag w-full py-4 ${data.tag.tagColor === 'blue' ? 'bg-blue-600' : 'bg-green-600'} flex items-center justify-center`}>
+              <h3 className="text-sm font-semibold text-white">{data.tag.tagTitle}</h3>
+            </div>
+          )}
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -1,8 +1,12 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-key */
-import React from "react";
+import React, { useRef } from "react";
 import Card from "./Card";
 
 const Forground = () => {
+
+  const ref = useRef(null)
+
   const data = [
     {
       desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
@@ -10,12 +14,24 @@ const Forground = () => {
       close: true,
       tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" },
     },
+    {
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "blue" },
+    },
+    {
+      desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+      filesize: ".9mb",
+      close: true,
+      tag: { isOpen: false, tagTitle: "Upload", tagColor: "green" },
+    },
   ];
   return (
     <div>
-      <div className="fixed z-[3] top-0 left-0 w-full h-full ">
+      <div ref={ref} className="fixed z-[3] top-0 left-0 w-full h-full flex gap-10 flex-wrap p-5">
         {data.map((item, index)=>(
-            <Card data={item} />
+            <Card data={item} reference={ref} />
         ))}
       </div>
     </div>
